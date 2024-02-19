39a96fcf311d255c69b29fc787ddaffb8a12079e
GowriChelluri <gowrichelluri08@gmail.com>
2024-02-19 14:36:30 +0000

    Commited

---
+++ src/components/Home/index.js
@@ -0,0 +1,12 @@
+import './index.css'
+import UserInfo from '../UserInfo'
+import BlogsList from '../BlogsList'
+
+const Home = () => (
+  <div className="home-container">
+    <UserInfo />
+    <BlogsList />
+  </div>
+)
+
+export default Home
